482800baf7ff72f9864eb8602cb47031d6a343f0
chanchal16 <rajputchanchal17@gmail.com>
2022-05-09 13:55:28 +0530

    Create routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import {Login,SignUp} from './pages';
+
+const ROUTES = [
+    {
+        path:"/login",
+        element:<Login />
+    },
+    {
+        path:"/signup",
+        element:<SignUp/>
+    },
+]
+export {ROUTES}
