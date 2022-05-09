fb6c1a08153532e3250c9506885109f516faf1bc
chanchal16 <rajputchanchal17@gmail.com>
2022-05-09 14:58:59 +0530

    Update - add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./public/index.html','./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      'gray':'#9ca3af',
+      'primary': '#fdba74',
+      'primary-dark':'#fb923c',
+      'pista':'#d1e3ca',
+      'pista-dark':'#AFD89F'
+    },
     extend: {},
   },
   variants: {
