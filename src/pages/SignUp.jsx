2744705c8d284386276ce9b08c236dd2206df22b
chanchal16 <rajputchanchal17@gmail.com>
2022-05-09 14:59:20 +0530

    Update - changed color

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ import { SignupUser } from '../services/auth-services';
 
 export const SignUp = () => {
     const navigate= useNavigate()
-    const {authDispatch,authState} = useAuth()
+    const {authDispatch} = useAuth()
     const [signUpForm,setSignUpForm] = useState({name:'',email:'',password:''})
 
     // handle submit
@@ -20,32 +20,32 @@ export const SignUp = () => {
         }))
         navigate('/login')
     }
-    console.log('user',authState)
+
   return (
     <div className="container mx-auto px-1">
-        <form className='max-w-sm my-12 mx-auto p-4 border-2 border-pink-300 
+        <form className='max-w-sm my-12 mx-auto p-4 border-2 border-primary 
         border-solid flex flex-col justify-center items-center gap-4' onSubmit={ handleSubmit}>
-            <h1 className="text-2xl text-pink-300">Sign Up</h1>
-            <div className="flex flex-col text-sm text-slate-400 w-10/12 ">
+            <h1 className="text-2xl text-primary">Sign Up</h1>
+            <div className="flex flex-col text-sm text-gray w-10/12 ">
                 <label>Name</label>
                 <input type="text" placeholder="enter name" className="p-2 outline outline-1" value={signUpForm.name}
                 onChange={(e)=>setSignUpForm((form)=>({...form,name:e.target.value}))} />
             </div>
-            <div className="flex flex-col text-sm text-slate-400 w-10/12 ">
+            <div className="flex flex-col text-sm text-gray w-10/12 ">
                 <label>Email</label>
                 <input type="email" placeholder="abc@gmail.com" className="p-2 outline outline-1" value={signUpForm.email}
                 onChange={(e)=>setSignUpForm((form)=>({...form,email:e.target.value}))} />
             </div>
-            <div className="flex flex-col text-sm text-slate-400 w-10/12 ">
+            <div className="flex flex-col text-sm text-gray w-10/12 ">
                 <label>Password</label>
                 <input type="password" className="p-2 outline outline-1" value={signUpForm.password}
                 onChange={(e)=>setSignUpForm((form)=>({...form,password:e.target.value}))} />
             </div>
-            <button type='submit' className="p-1.5 w-10/12 text-white rounded border-none bg-pink-300 
-            hover:bg-pink-400" onClick={handleSignUp}>
+            <button type='submit' className="p-1.5 w-10/12 text-white rounded border-none bg-primary 
+            hover:bg-primary-dark" onClick={handleSignUp}>
                 Sign Up
             </button>  
-            <p>Already have an account ?<Link to='/login' className="text-orange-300">Sign in</Link></p>            
+            <p>Already have an account ?<Link to='/login' className="text-primary-dark">Sign in</Link></p>            
         </form>
     </div>
   )
