7b58cc4ac2cb1c18606479efd9fcd67fe81b0432
chanchal16 <rajputchanchal17@gmail.com>
2022-05-10 12:13:38 +0530

    Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     colors:{
       'gray':'#9ca3af',
+      'white': '#ffffff',
       'primary': '#fdba74',
       'primary-dark':'#fb923c',
       'pista':'#d1e3ca',
