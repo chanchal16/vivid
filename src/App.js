dd8b0fa7edee0eaf816226b4e8c6ca441ad86584
chanchal16 <rajputchanchal17@gmail.com>
2022-05-10 12:12:38 +0530

    Update - add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { useRoutes } from "react-router-dom";
+import { Navbar } from "./components/common/Navbar";
 import { ROUTES } from "./routes";
 
 function App() {
   const routeElement = useRoutes(ROUTES)
   return (
     <div className='App'>
+      <Navbar/>
       <main>
       {routeElement}
       </main>
