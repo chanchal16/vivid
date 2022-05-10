fb264d7cb5ac14d4d107ecf63dee055861cddff4
chanchal16 <rajputchanchal17@gmail.com>
2022-05-10 12:12:47 +0530

    Create Navbar.jsx

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import { MdOutlineExplore,  MdAddCircleOutline, MdOutlineBookmarkBorder, MdPersonOutline } from 'react-icons/md';
+import {GiAzulFlake} from "react-icons/gi";
+import {HiOutlineHome} from "react-icons/hi";
+
+export const Navbar = () => {
+    const {authState} = useAuth();
+    
+  return (
+    <div className="app-container flex items-center md:justify-between p-2.5 bg-white text-lg">
+       {/* hide on mobile */}
+        <NavLink to="/" className="app-navbar-logo hidden md:block">
+            <span className="flex items-center text-primary hover:text-pista">
+                <GiAzulFlake className="mr-1 " />
+                Vivid
+            </span>
+        </NavLink>
+        <div className="flex items-center flex-grow md:justify-end">
+            <ul className="flex flex-grow md:flex-initial items-center justify-between md:gap-x-4 flex-wrap">
+                <li>
+                    <NavLink end to="/" className="flex items-center" aria-label="Home">
+                        <HiOutlineHome size='1.5rem' />
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink end to="/explore" className="flex items-center" aria-label="Search">
+                        <MdOutlineExplore size='1.5rem' />
+                    </NavLink>
+                </li>              
+                    <li>
+                        <NavLink end to="/addpost" className="flex items-center" aria-label="Add post">
+                            <MdAddCircleOutline size='1.5rem' />
+                        </NavLink>
+                    </li>            
+                <li>
+                    <NavLink end to="/bookmarks" className="flex items-center relative" aria-label="Notifications">
+                        <MdOutlineBookmarkBorder size='1.5rem' />
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink end to={"/profile/"} className="flex items-center profile-pic" aria-label="User Profile">
+                        {
+                            authState?.userAvatar ? 
+                            <img src={authState?.userAvatar} alt="User" />
+                            :
+                            <MdPersonOutline size='1.5rem' />
+                        }
+                    </NavLink>
+                </li>
+
+            </ul>
+        </div> 
+    </div>
+  )
+}
