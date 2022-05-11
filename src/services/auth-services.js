05f4fbe6d47b15cfa11a99df6bae0b30d47d095c
chanchal16 <rajputchanchal17@gmail.com>
2022-05-11 20:25:47 +0530

    refactor services

--- src/services/auth-services.js
+++ src/services/auth-services.js
@@ -1,39 +1,11 @@
 import axios from "axios";
-
-const LoginUser = async(email,password,dispatch)=>{
-    try{
-        const {
-            data: { foundUser, encodedToken }
-          } = await axios.post("api/auth/login",{email,password})
-        const user = {
-            token:encodedToken,
-            user:foundUser
-        }
-        localStorage.setItem('user',JSON.stringify(user))
-        dispatch({type:'UPDATE_USER',payload:user})
-    }catch(err){
-        localStorage.clear();
-        dispatch({type:'FAILURE',payload:err})
-        console.error('Cannot login',err)
-    }
+// login user
+const LoginUser = async(email,password)=>{
+    return await axios.post("api/auth/login",{email,password})
 }
 
 // signup user
-const SignupUser = async(name,email,password,dispatch)=>{
-    try{
-        const {
-            data: { createdUser, encodedToken }
-          } = await axios.post("api/auth/signup",{name,email,password})
-        const user = {
-            token:encodedToken,
-            user:createdUser
-        }
-        localStorage.setItem('user',JSON.stringify(user))
-        dispatch({type:'UPDATE_USER',payload:user})
-    }catch(err){
-        localStorage.clear();
-        dispatch({type:'FAILURE',payload:err})
-        console.error('Cannot sign-up,please try again later',err)
-    }
+const SignupUser = async(name,email,password)=>{
+    return await axios.post("api/auth/signup",{name,email,password})
 }
 export{LoginUser,SignupUser}
