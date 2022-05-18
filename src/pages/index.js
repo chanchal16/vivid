a7469de37ffea2c35f05d486d178115be4c31a62
chanchal16 <rajputchanchal17@gmail.com>
2022-05-18 23:46:23 +0530

    chore - export explore page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 export {Login} from './Login';
 export {SignUp} from './SignUp';
 export {Feed} from './Feed';
-export {Bookmarks} from './Bookmarks'
+export {Explore} from './Explore';
+export {Bookmarks} from './Bookmarks';
+export {PostPage} from './PostPage';
