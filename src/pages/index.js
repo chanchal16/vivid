4b2cf9936cb33b1a5bcfdb307c60cafda7dce5bf
chanchal16 <rajputchanchal17@gmail.com>
2022-05-20 23:59:47 +0530

    chore- export profile page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export {Login} from './Login';
 export {SignUp} from './SignUp';
 export {Feed} from './Feed';
-export {Bookmarks} from './Bookmarks'
+export {Bookmarks} from './Bookmarks';
+export {Profile} from './Profile'
