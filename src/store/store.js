2ed5350d5da4c0508c4365dc40a26e215b376362
chanchal16 <rajputchanchal17@gmail.com>
2022-05-21 00:03:06 +0530

    Update - add users reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from 'features/authentication/authSlice';
-import postReducer from 'features/posts/postSlice'
+import postReducer from 'features/posts/postSlice';
+import profileReducer from 'features/profile/profileSlice';
 
 export const store = configureStore({
   reducer: {
     auth:authReducer,
-    post:postReducer
+    post:postReducer,
+    users:profileReducer
   },
 })
