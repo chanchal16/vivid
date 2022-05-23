c8d3b76ed8b5c73546c073c84ab83e53efb1ebbe
chanchal16 <rajputchanchal17@gmail.com>
2022-05-23 19:29:12 +0530

    Update - add follow/unfollow handlers

--- src/features/profile/profileSlice.js
+++ src/features/profile/profileSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
 
 // get all users
 export const getAllUsers = createAsyncThunk(
@@ -8,7 +7,6 @@ export const getAllUsers = createAsyncThunk(
 	async () => {
 		try {
 			const {data} = await axios.get('/api/users');
-			console.log('data-of-all-users',data)
 			return data.users;
 		} catch (err) {
 			console.error(err);
@@ -19,8 +17,7 @@ export const getAllUsers = createAsyncThunk(
 // get user
 export const getUser = createAsyncThunk("users/getUser",async(username)=>{
 	try{
-		const {data} = await axios.get(`/api/users/${username}`)
-		
+		const {data} = await axios.get(`/api/users/${username}`)		
 		return data.user
 	}catch(err){console.error(err)}
 })
@@ -43,14 +40,29 @@ export const getUserPost = createAsyncThunk(
 	"users/getUserPost",
 	async ( username ) => {
 		try {
-			const {data} = await axios.get(`/api/posts/user/${username}`);
-			console.log('data',data)
+			const {data} = await axios.get(`/api/posts/user/${username}`);			
 			return data.posts;
 		} catch (err) {
 			console.error(err)
 		}
 	}
 );
+// follow user
+export const followUser = createAsyncThunk("users/followUser",async({token,userId})=>{
+	try{
+		const {data} = await axios.post(`/api/users/follow/${userId}`,{},
+		{headers:{authorization:token}})
+		return data
+	}catch(err){console.error(err)}
+})
+// unfollow user
+export const unFollowUser = createAsyncThunk("users/unFollowUser",async({token,userId})=>{
+	try{
+		const {data} = await axios.post(`/api/users/unfollow/${userId}`,{},
+		{headers:{authorization:token}})
+		return data
+	}catch(err){console.error(err)}
+})
 
 const initialState = {
 	userProfileToShow: {},
@@ -106,7 +118,25 @@ const profileSlice = createSlice({
 		.addCase(updateUser.rejected,(state)=>{
 			state.profileStatus = 'Rejected';
 		})
-		
+		// follow user
+		.addCase(followUser.fulfilled,(state,action)=>{
+			state.profileStatus = 'Fulfilled'
+			const{user,followUser} = action.payload
+			state.allUsers = state.allUsers.map(curruser=>curruser.username === user.username ? user :
+				curruser.username === followUser.username ? followUser : curruser)
+			state.userProfileToShow = action.payload.followUser;
+		})
+		.addCase(followUser.rejected,(state)=>{
+			state.profileStatus = 'Rejected'
+		})
+		.addCase(unFollowUser.fulfilled,(state,action)=>{
+			state.profileStatus = 'Fulfilled'
+			const{user,followUser} = action.payload
+			state.allUsers = state.allUsers.map(curruser=>curruser.username === user.username ? user :
+				curruser.username === followUser.username ? followUser : curruser)
+			
+			state.userProfileToShow = action.payload.followUser;
+		})		
 	  }
 });
 
