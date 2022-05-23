32792ed219acc51be6bb808821f03b30cda8e8ab
chanchal16 <rajputchanchal17@gmail.com>
2022-05-23 19:29:56 +0530

    Update - add follow/unfollow handler

--- src/features/profile/UserProfileInfo.jsx
+++ src/features/profile/UserProfileInfo.jsx
@@ -1,20 +1,50 @@
 import React from 'react'
 import { FaBirthdayCake, FaMapMarkerAlt} from 'react-icons/fa';
+import { useSelector,useDispatch } from 'react-redux';
+import { followUser, unFollowUser } from './profileSlice';
+import { useParams} from "react-router-dom";
 
 export const UserProfileInfo = ({userData}) => {
+    const {user} = useSelector(state => state.auth)
+    const{token} = user
+    const {username} = useParams();
+    const dispatch = useDispatch()
+    // check if following 
+    const isFollowing = userData?.followers.some(user=>user.username !== username)
+
+    const followUnfollowHandler = ()=>{
+        if(isFollowing){
+            dispatch(unFollowUser({token,userId:userData._id}))
+        }else{
+            dispatch(followUser({token,userId:userData._id}))
+        }
+    }
 
   return (
     <div className="p-2 md:px-4 md:py-2">
         <div className="flex justify-between">
             <div className="">
-                <h3 className="text-gray text-xl mb-1">{userData.name}</h3>
-                <h3 className="text-gray text-md mb-1">@{userData.username}</h3>
+                <h3 className=" text-xl mb-1">{userData.name}</h3>
+                <h3 className="text-gray-dark text-md mb-1">@{userData.username}</h3>
             </div>
-
+            {
+                user?.user.username !== userData.username ? (
+                    <div>
+                        <button className="bg-primary p-1 rounded hover:bg-pista-dark"
+                        onClick={followUnfollowHandler}>
+                            {isFollowing ? 'unfollow' : 'follow'}
+                        </button>
+                    </div>
+                    
+                ) :(
+                    null
+                )
+            }
         </div>
         <p className="text-gray text-sm">
             {userData.bio}
         </p>
+        
         <div className="flex my-2 gap-4 flex-wrap">
         {
             userData.portfolio &&
