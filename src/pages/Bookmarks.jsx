59093970c61503737ab31362d39c08f9f147bbaf
chanchal16 <rajputchanchal17@gmail.com>
2022-05-24 17:47:28 +0530

    Update Bookmarks.jsx

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { PostCard } from 'components/common/PostCard';
-import React from 'react'
 import {useDispatch, useSelector } from 'react-redux';
 import { getBookmarks } from 'features/posts/postSlice';
+import nodata from 'assets/nodata.svg'
+import { Link } from 'react-router-dom';
 
 export const Bookmarks = () => {
     const authState = useSelector(state => state.auth);
@@ -21,11 +22,24 @@ export const Bookmarks = () => {
     
   return (
     <div className='container mx-auto w-9/12 m-4 p-6'>
+      {BookmarkedPosts.length > 0 ? (
+        <>
         {
-            BookmarkedPosts?.map(post=>(
-                <PostCard post={post} key={post?._id}/>
-            ))
-        }
+          BookmarkedPosts?.map(post=>(
+              <PostCard post={post} key={post?._id}/>
+          ))
+        }</>
+      ):(
+        <div className='flex flex-col justify-center items-center'>
+        <img src={nodata} alt='liked' className='w-72' />
+        <p className='text-sm text-gray text-center p-4'>You have not saved any posts yet.</p>
+        <Link to='/'>
+            <button className="m-auto bg-primary p-1 rounded hover:bg-primary-dark">explore</button>
+        </Link>
+      </div>
+      )
+    }
+        
     </div>
   )
 }
