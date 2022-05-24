03eea9da8ef0dc62fbef5f0ef13713d076232770
chanchal16 <rajputchanchal17@gmail.com>
2022-05-24 17:46:26 +0530

    Update - posts details

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -16,19 +16,22 @@ export const posts = [
       likedBy: [],
       dislikedBy: [],
     },
-    img:{},
+    postImg:{},
     comments:[],
     username: "adarshbalika",
     name:'Adarsh Balika',
     avatarUrl:'https://res.cloudinary.com/cr07/image/upload/v1591170324/samples/people/bicycle.jpg',
     createdAt: formatDate(),
-    updatedAt: formatDate(),
+    updatedAt: '2022-05-14T16:42:09+05:30',
   },
   {
     _id: uuid(),
     content:
       "",
-    img:'https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk',
+    postImg:{
+      original_filename:'',
+      url:'https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk'
+    },
     likes: {
       likeCount: 2,
       likedBy: [],
@@ -39,7 +42,7 @@ export const posts = [
     name:'Chanchal Rajput',
     avatarUrl:'https://res.cloudinary.com/cr07/image/upload/v1591779540/uzo2kczlerzdc8t9abin.jpg',
     createdAt: formatDate(),
-    updatedAt: formatDate(),
+    updatedAt: '2022-05-04T16:42:09+05:30',
   },
   {
     _id: uuid(),
@@ -61,7 +64,10 @@ export const posts = [
     _id: uuid(),
     content:
       "Being alone is peaceful!",
-    img:'https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk',
+    postImg:{
+      original_filename:'',
+      url:'https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk'
+    },
     likes: {
       likeCount: 7,
       likedBy: [],
@@ -94,7 +100,7 @@ export const posts = [
     _id: uuid(),
     content: `Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you
        are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.`,
-    img:{},
+    postImg:{},
     likes: {
       likeCount: 5,
       likedBy: [],
@@ -105,7 +111,7 @@ export const posts = [
     name:'Adarsh Balika',
     avatarUrl:'https://res.cloudinary.com/cr07/image/upload/v1591170324/samples/people/bicycle.jpg',
     createdAt: formatDate(),
-    updatedAt: formatDate(),
+    updatedAt: '2022-04-29T16:42:09+05:30',
   },
   {
     _id: uuid(),
@@ -121,6 +127,6 @@ export const posts = [
     username:'tester',
     avatarUrl:'https://res.cloudinary.com/cr07/image/upload/v1591170323/samples/people/boy-snow-hoodie.jpg',
     createdAt: formatDate(),
-    updatedAt: formatDate(),
+    updatedAt: '2022-05-21T16:42:09+05:30',
   }
 ];
