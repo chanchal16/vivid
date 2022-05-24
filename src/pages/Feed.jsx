b514373dbdad65de9f3408d5eb2c92ea960cccbb
chanchal16 <rajputchanchal17@gmail.com>
2022-05-24 17:48:00 +0530

    Update - add sort functionality

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,25 +1,31 @@
-import { getPosts, SHOW_MODAL } from 'features/posts/postSlice';
+import { getPosts, SHOW_MODAL, SORT_POST_TYPE } from 'features/posts/postSlice';
 import { SuggestedUsers } from 'features/profile/SuggestedUsers';
 import React,{useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { getUserFeed,sortPosts } from 'utils';
 import { PostCard } from '../components/common/PostCard';
 
 export const Feed = () => {
+  const authState = useSelector(state=>state.auth);
+  let {user} = authState
     const postState = useSelector(state => state.post);
+    let {sortPostType} = postState
     const dispatch = useDispatch() 
-    console.log('posts',postState?.allPosts)
-    // sort by recently added
-    const sortedPosts = postState?.allPosts?.slice().reverse()
+
+    const userFeed = getUserFeed(postState?.allPosts,user?.user?.following,user?.user?.username).slice().reverse()
+    const sortedPosts = sortPosts(userFeed,sortPostType)
 
     useEffect(() => {
         dispatch(getPosts())
     }, [dispatch])
+
+    const sortClickHandler = (type)=>{
+      dispatch(SORT_POST_TYPE(type))
+    }
     
   return (
     <div className='grid grid-cols-6 gap-x-4 lg:gap-x-8 w-full my-4'>
-
-        <div className="col-span-6 md:col-span-4">
-            {/* <CreatePost/>  */}
+        <div className="col-span-6 md:col-span-4">           
             <div className="flex bg-white mb-2 p-4 rounded">
               <img
                 className="w-12 rounded-full mr-2"
@@ -32,10 +38,24 @@ export const Feed = () => {
                 What's on your mind?
               </button>
             </div>
+            <div className='m-4 grid grid-cols-6'>
+              <button className={`outline-none col-span-2 border-b-4 p-2 ${sortPostType === 'SORT_BY_RECENT'?' border-primary':'default'}`} 
+                onClick={()=>sortClickHandler('SORT_BY_RECENT')}>
+                recent
+              </button>
+              <button className={`outline-none col-span-2 border-b-4 p-2 ${sortPostType === 'SORT_BY_OLDER'?' border-primary':'default'}`} 
+                onClick={()=>sortClickHandler('SORT_BY_OLDER')}>
+                older
+              </button>
+              <button className={`outline-none col-span-2 border-b-4 p-2 ${sortPostType === 'TRENDING'?' border-primary':'default'}`} 
+                onClick={()=>sortClickHandler('TRENDING')}>
+                trending
+              </button>              
+            </div>
             {
-                sortedPosts?.map(post=>(
-                    <PostCard post={post} key={post?._id}/>
-                ))
+              sortedPosts?.map(post=>(
+                  <PostCard post={post} key={post?._id}/>
+              ))
             }
         </div>
         <div className="col-span-6 md:col-span-2">
