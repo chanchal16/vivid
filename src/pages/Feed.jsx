362c42c1050e8adf56957a8bf31f0a02ddefaf17
chanchal16 <rajputchanchal17@gmail.com>
2022-06-02 15:08:59 +0530

    Update - add clean up function

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,23 +1,35 @@
-import { getPosts, SHOW_MODAL, SORT_POST_TYPE } from 'features/posts/postSlice';
+import { getPosts,SHOW_MODAL, SORT_POST_TYPE } from 'features/posts/postSlice';
 import { SuggestedUsers } from 'features/profile/SuggestedUsers';
-import React,{useEffect} from 'react'
+import React,{useEffect,useCallback,useRef} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserFeed,sortPosts } from 'utils';
 import { PostCard } from '../components/common/PostCard';
 
-export const Feed = () => {
+export const Feed = ({setShowEmojiContainer}) => {
   const authState = useSelector(state=>state.auth);
   let {user} = authState
     const postState = useSelector(state => state.post);
     let {sortPostType} = postState
     const dispatch = useDispatch() 
-
     const userFeed = getUserFeed(postState?.allPosts,user?.user?.following,user?.user?.username).slice().reverse()
     const sortedPosts = sortPosts(userFeed,sortPostType)
+    const mountedRef = useRef(true)
 
-    useEffect(() => {
+    const getAllPosts= useCallback(
+      () => {
+        if(!mountedRef.current) return null
         dispatch(getPosts())
-    }, [dispatch])
+      },
+      [mountedRef],
+    )
+    
+    useEffect(() => {
+       getAllPosts()
+        return(()=>{
+          mountedRef.current = false
+          setShowEmojiContainer(false)
+        })
+    }, [])
 
     const sortClickHandler = (type)=>{
       dispatch(SORT_POST_TYPE(type))
@@ -33,7 +45,7 @@ export const Feed = () => {
                 alt="user"
               />
               <button
-                className="rounded-full w-full border-2 border-light border-solid text-left px-5 text-gray"
+                className="rounded-full w-full border-2 border-light border-solid text-left px-5 text-gray cursor-text"
                 onClick={() => dispatch(SHOW_MODAL())}>
                 What's on your mind?
               </button>
