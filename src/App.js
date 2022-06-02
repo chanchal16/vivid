fb67f093965402aaa741c8ddfe7eb774f52ed0ce
chanchal16 <rajputchanchal17@gmail.com>
2022-06-02 15:02:31 +0530

    Update - add state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import {useState} from 'react'
 import { PostModal } from "features/posts/PostModal";
 import { useRoutes } from "react-router-dom";
 import { Navbar } from "./components/common/Navbar";
 import { ROUTES } from "./routes";
 
 function App() {
+  const [showEmojiContainer,setShowEmojiContainer] = useState(false);
   const routeElement = useRoutes(ROUTES)
   return (
     <div className='App'>
       <Navbar/>
       <main className='app-container'>
       {routeElement}
-       <PostModal/>
+       <PostModal showEmojiContainer={showEmojiContainer} setShowEmojiContainer={setShowEmojiContainer} />
       </main>
     </div>
   );
