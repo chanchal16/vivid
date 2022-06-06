c9b9717a539311d88ac757087919cd5a17716d11
chanchal16 <rajputchanchal17@gmail.com>
2022-06-06 22:01:47 +0530

    Update - add reducers for follow

--- src/features/authentication/authSlice.js
+++ src/features/authentication/authSlice.js
@@ -1,10 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { followUser, unFollowUser } from 'features/profile/profileSlice'
 
 const initialState = {
     user: null || JSON.parse(localStorage.getItem('user')),
     error: "",
 }
-
+// check the followings of usr
+export let userFollowing = initialState?.user?.user?.following
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -24,6 +26,15 @@ export const authSlice = createSlice({
       state.error = action.payload
     },
   },
+  extraReducers: (builder) => {
+    builder
+    .addCase(followUser.fulfilled,(state,action)=>{
+      userFollowing = action.payload.user
+    })
+    .addCase(unFollowUser.fulfilled,(state,action)=>{
+      userFollowing = action.payload.user
+    })
+  }
 })
 
 // Action creators are generated for each case reducer function
