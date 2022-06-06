0a3875d6aa038d82bf48044508c574d8d8255da3
chanchal16 <rajputchanchal17@gmail.com>
2022-06-06 13:34:04 +0530

    Update - lazy loaded modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import {useState} from 'react'
-import { PostModal } from "features/posts/PostModal";
+import React,{useState,Suspense} from 'react'
 import { useRoutes } from "react-router-dom";
 import { Navbar } from "./components/common/Navbar";
 import { ROUTES } from "./routes";
+const PostModal = React.lazy(()=>import("features/posts/PostModal"));
 
 function App() {
   const [showEmojiContainer,setShowEmojiContainer] = useState(false);
@@ -12,7 +12,9 @@ function App() {
       <Navbar/>
       <main className='app-container'>
       {routeElement}
-       <PostModal showEmojiContainer={showEmojiContainer} setShowEmojiContainer={setShowEmojiContainer} />
+      <Suspense fallback={<div></div>}>
+        <PostModal showEmojiContainer={showEmojiContainer} setShowEmojiContainer={setShowEmojiContainer} />
+      </Suspense>
       </main>
     </div>
   );
