8442f426fc10e8aa1e6b33e72a66a1dc9a7738ea
chanchal16 <rajputchanchal17@gmail.com>
2022-06-07 17:30:32 +0530

    refactor

--- src/server.js
+++ src/server.js
@@ -46,8 +46,6 @@ export function makeServer({ environment = "development" } = {}) {
       users.forEach((item) =>
         server.create("user", {
           ...item,
-          followers: [],
-          following: [],
           bookmarks: [],
         })
       );
