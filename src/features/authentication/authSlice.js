e8af66599d4d377ab7d7b17d1e576f7f03e81407
chanchal16 <rajputchanchal17@gmail.com>
2022-06-07 17:26:03 +0530

    Update - removed unused code

--- src/features/authentication/authSlice.js
+++ src/features/authentication/authSlice.js
@@ -1,12 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { followUser, unFollowUser } from 'features/profile/profileSlice'
 
 const initialState = {
     user: null || JSON.parse(localStorage.getItem('user')),
     error: "",
 }
-// check the followings of usr
-export let userFollowing = initialState?.user?.user?.following
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -26,15 +24,6 @@ export const authSlice = createSlice({
       state.error = action.payload
     },
   },
-  extraReducers: (builder) => {
-    builder
-    .addCase(followUser.fulfilled,(state,action)=>{
-      userFollowing = action.payload.user
-    })
-    .addCase(unFollowUser.fulfilled,(state,action)=>{
-      userFollowing = action.payload.user
-    })
-  }
 })
 
 // Action creators are generated for each case reducer function
