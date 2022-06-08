80e7cfa4ddea329a012c0f0d57d9bf555737aa51
chanchal16 <rajputchanchal17@gmail.com>
2022-06-08 15:25:35 +0530

    chore - add env variables

--- src/features/posts/PostModal.jsx
+++ src/features/posts/PostModal.jsx
@@ -25,14 +25,14 @@ const PostModal = ({showEmojiContainer,setShowEmojiContainer}) => {
       try{
         const data = new FormData();
         data.append("file", e.target?.files[0]);
-        data.append("upload_preset",'fryrvzcc');
-        data.append("cloud_name","cr07")
+        data.append("upload_preset",process.env.REACT_APP_UPLOAD_PRESET);
+        data.append("cloud_name",process.env.REACT_APP_CLOUD_NAME)
         const requestOptions = {
           method: "POST",
           body: data,
         };
         const response = await fetch(
-          "https://api.cloudinary.com/v1_1/cr07/image/upload",
+          `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`,
           requestOptions
         )
         const { url, original_filename } = await response.json();
