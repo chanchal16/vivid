df30b967695bb31c322c96effe2470849649842e
chanchal16 <rajputchanchal17@gmail.com>
2022-06-08 13:39:50 +0530

    Update - add follow/unfollow functionality

--- src/components/common/UserCard.jsx
+++ src/components/common/UserCard.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from 'features/profile/profileSlice';
 
-export const UserCard = ({user,close}) => {   
-     
+export const UserCard = ({User,close}) => { 
+    let {user} = useSelector(state => state.auth);
+    let{token} = user
+    const dispatch = useDispatch()
+    const isFollowing = user?.user?.following?.includes( User?.username)
+
+    const followUnfollowHandler = (userId)=>{
+        if(isFollowing){
+            dispatch(unFollowUser({token,userId}))
+        }else{
+            dispatch(followUser({token,userId}))
+        }
+    }
   return (
     <div className="bg-white p-2 rounded flex gap-x-2 shadow-sm justify-between items-center">
         <div className="flex gap-x-2 flex-grow">
-            <Link to={"/profile/"+user.username} aria-label={user.name} onClick={close}>
+            <Link to={"/profile/"+User?.username} aria-label={User?.name} onClick={close}>
                 <img
                 className="w-12 h-12 rounded-full"
-                src={  user.avatarUrl }
-                alt={user.username}
+                src={  User?.avatarUrl }
+                alt={User?.username}
                 />
             </Link>
                        
             <div className="flex flex-col">
-                <Link to={"/profile/"+user.username} aria-label={user.name} onClick={close}
+                <Link to={"/profile/"+User?.username} aria-label={User?.name} onClick={close}
                 className='text-primary-dark hover:text-pista-dark' >
-                    <h3>{user.name}</h3>
+                    <h3>{User?.name}</h3>
                 </Link>
-                <small className="text-gray-dark">@{user.username}</small>
+                <small className="text-gray-dark">@{User?.username}</small>
             </div>
+            <button className='text-sm self-start py-0.5 text-primary-dark ml-auto hover:text-pista-dark'
+            onClick={()=>followUnfollowHandler(User?._id)} >
+                {isFollowing ? 'unfollow' : 'follow'}
+            </button>
         </div>
     </div>
   )
