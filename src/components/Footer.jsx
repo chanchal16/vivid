37954b23c5ef6c83d5af483a5b0093cb69b7b7a9
chanchal16 <rajputchanchal17@gmail.com>
2022-06-08 21:30:49 +0530

    Create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="flex  p-2 text-sm text-primary-dark border-t border-primary ">
+        <p className="text-center w-full">&lt;/&gt; by Chanchal.</p>
+    </footer>
+  )
+}
